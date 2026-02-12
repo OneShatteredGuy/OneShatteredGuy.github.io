20638ddc1d9277344c37e0ea3ef301a6700f4fc3
OneShatteredGuy <oneshatteredguy@gmail.com>
2026-02-11 19:01:44 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const FIREBASE_API_KEY = "AIzaSyCfJxEQZ19bCXWzPk9xPciHCex1-04luxs";
+export const FIREBASE_AUTH_DOMAIN = "portfolio-website-4972e.firebaseapp.com";
+export const FIREBASE_PROJECT_ID = "portfolio-website-4972e";
+export const FIREBASE_STORAGE_BUCKET = "portfolio-website-4972e.firebasestorage.app";
+export const FIREBASE_MESSAGING_SENDER_ID = "676835549264";
+export const FIREBASE_APP_ID = "1:676835549264:web:69773fbbd6bd78e5dd3155";
+export const FIREBASE_MEASUREMENT_ID = "G-33L16YM3PF";
+
+export const YOUTUBE_API_KEY = "AIzaSyALfITPDaCn2UIF9vfkizOQERXNqf4lACg";
